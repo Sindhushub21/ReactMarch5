ffbdb36244adf09ffea60c179d2bfaad61e458c3
Jaxon Ray Petersen <thatCashBar@gmail.com>
2021-04-04 12:52:03 -0400

    Updated Contact Us Page, Added Email

--- ClientApp/src/components/Contact.js
+++ ClientApp/src/components/Contact.js
@@ -13,16 +13,23 @@ export class Contact extends Component {
                 <Card>
                     <Card.Header as="h5" style={{ textAlign: 'center'}}>Phone Numbers</Card.Header>
                     <Card.Body>
-                        <p>Main Office: <span style={{ float: 'right' }}>123-007-1337</span></p>
-                        <p>Service: <span style={{ float: 'right' }}>456-007-1337</span></p>
-                        <p>Sales: <span style={{ float: 'right' }}>789-007-1337</span></p>
+                        <p style={{fontWeight: '500'}}>Main Office: <span style={{ float: 'right', fontWeight: 'normal' }}>123-007-1337</span></p>
+                        <p style={{fontWeight: '500'}}>Service: <span style={{ float: 'right', fontWeight: 'normal' }}>456-007-1337</span></p>
+                        <p style={{fontWeight: '500'}}>Sales: <span style={{ float: 'right', fontWeight: 'normal' }}>789-007-1337</span></p>
                     </Card.Body>
                 </Card>
                 <br/>
                 <Card>
-                    <Card.Header as="h5" style={{ textAlign: 'center'}}>Find Us</Card.Header>
+                    <Card.Header as="h5" style={{ textAlign: 'center'}}>Email</Card.Header>
                     <Card.Body style={{ textAlign: 'center'}}>
-                        Address: 808 W Dealership Dr, Smallville, KS, 67524
+                        John.Smith@CarDealer.com
+                    </Card.Body>
+                </Card>
+                <br/>
+                <Card>
+                    <Card.Header as="h5" style={{ textAlign: 'center'}}>Locations</Card.Header>
+                    <Card.Body style={{ textAlign: 'center'}}>
+                        <span style={{fontWeight: '500'}}>Main:</span> 808 W Dealership Dr, Smallville, KS, 67524
                     </Card.Body>
                 </Card>
             </CardGroup>
